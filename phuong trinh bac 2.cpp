477a92b69473a03d1a120ae0c9e9de7456de2071
midoricsmile <midoricsmile@gmail.com>
2016-08-31 22:21:24 +0700

    a
    a

---
+++ phuong trinh bac 2.cpp
@@ -0,0 +1,17 @@
+// phuong trinh bac 2.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+#include <iostream>
+#include "PTB2.h"
+using namespace std;
+
+int _tmain(int argc, _TCHAR* argv[])
+{
+	
+	PTB2 A1;
+	A1.kiemtra();
+	//cout << "nguhiem " << A1.kiemtra();
+	return 0;
+}
+
